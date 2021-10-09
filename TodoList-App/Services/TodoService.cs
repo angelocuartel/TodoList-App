47dd8ee808ce52bbe9f0b8b39d10b98ef6c60bfe
angelocuartel <68045263+angelocuartel@users.noreply.github.com>
2021-10-09 12:57:37 +0800

    add todo service

---
+++ TodoList-App/Services/TodoService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList_App.Data;
+
+namespace TodoList_App.Services
+{
+    public class TodoService
+    {
+        private readonly AppDbContext _dbContext;
+        public TodoService(AppDbContext dbcontext)
+        {
+            _dbContext = dbcontext;
+        }
+    }
+}
