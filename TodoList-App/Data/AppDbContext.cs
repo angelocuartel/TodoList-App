cf96d0994fab5af187ece580504cc5c2959586fa
angelocuartel <68045263+angelocuartel@users.noreply.github.com>
2021-10-09 12:41:32 +0800

    create dbcontext

---
+++ TodoList-App/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList_App.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options)
+            :base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+
+           // modelBuilder.ApplyConfiguration();
+        }
+    }
+}
