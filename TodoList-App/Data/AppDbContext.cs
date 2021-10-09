c5077e89ccbe3707813c9c4a5c53066198f8c498
angelocuartel <68045263+angelocuartel@users.noreply.github.com>
2021-10-09 12:43:30 +0800

    add Todo class dbset

--- TodoList-App/Data/AppDbContext.cs
+++ TodoList-App/Data/AppDbContext.cs
@@ -3,11 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TodoList_App.Models;
 
 namespace TodoList_App.Data
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Todo> Todos { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext>options)
             :base(options)
         {
