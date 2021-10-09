370284ba9d12e8f9cd9e2b8a30dc7c471d792688
angelocuartel <68045263+angelocuartel@users.noreply.github.com>
2021-10-09 12:59:32 +0800

    Add Idbservice interface

---
+++ TodoList-App/Services/IDbService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList_App.Services
+{
+    interface IDbService<T>
+    {
+
+        void CreateAsync();
+
+        T GetByIdAsync(int id);
+
+        IEnumerable<T> GetAllAsync();
+
+        void DeleteAsync();
+
+        void UpdateAsync();
+    }
+}
