1ac480b2af4be40011b7c158404b26f47f086ced
angelocuartel <68045263+angelocuartel@users.noreply.github.com>
2021-10-09 11:24:12 +0800

    push project to remote

---
+++ TodoList-App/Controllers/TodoAppController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList_App.Controllers
+{
+    public class TodoAppController : Controller
+    {
+
+
+        public IActionResult TodoMainView()
+        {
+            return View();
+        }
+    }
+}
