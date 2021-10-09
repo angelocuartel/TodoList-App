8ed5673fe8a79364060205a51f7516cbc52e0326
angelocuartel <68045263+angelocuartel@users.noreply.github.com>
2021-10-09 12:46:43 +0800

    add Entityconfig to todo class

---
+++ TodoList-App/Data/EntityConfigurations/TodoEntityConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList_App.Models;
+
+namespace TodoList_App.Data.EntityConfigurations
+{
+    public class TodoEntityConfiguration : IEntityTypeConfiguration<Todo>
+    {
+        public void Configure(EntityTypeBuilder<Todo> builder)
+        {
+            builder.ToTable("todo_table");
+
+            builder.HasKey(i => i.TodoId);
+
+            builder.Property(p => p.TodoTask)
+                .HasColumnName("todo_task")
+                .HasMaxLength(255)
+                .IsRequired()
+                .HasColumnType("Varchar");
+        }
+    }
+}
