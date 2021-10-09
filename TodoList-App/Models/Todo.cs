31c105df2044772a6fc5da770a6fae6ccb23993e
angelocuartel <68045263+angelocuartel@users.noreply.github.com>
2021-10-09 12:42:27 +0800

    add todo class POCO

---
+++ TodoList-App/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList_App.Models
+{
+    public class Todo
+    {
+        public int TodoId { get; set; }
+        public string TodoTask { get; set; }
+
+    }
+}
