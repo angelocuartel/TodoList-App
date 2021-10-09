ef4653923245cf8c2bf0bd9a877e7c1721050ae2
angelocuartel <68045263+angelocuartel@users.noreply.github.com>
2021-10-09 12:54:23 +0800

    add migrations

---
+++ TodoList-App/Migrations/20211009045408_InitDatabase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TodoList_App.Migrations
+{
+    public partial class InitDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "todo_table",
+                columns: table => new
+                {
+                    TodoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    todo_task = table.Column<string>(type: "Varchar(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_todo_table", x => x.TodoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "todo_table");
+        }
+    }
+}
