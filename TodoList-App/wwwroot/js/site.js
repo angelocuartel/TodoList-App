8b3c42c6b49d684dd706efb8cf1de3965d85c6d3
angelocuartel <68045263+angelocuartel@users.noreply.github.com>
2021-10-25 15:52:27 +0800

    add client-side validation

--- TodoList-App/wwwroot/js/site.js
+++ TodoList-App/wwwroot/js/site.js
@@ -1,9 +1,25 @@
-﻿function showModal(urlPath, modalHolder) {
+﻿
+
+
+
+
+
+
+function showModal(urlPath, modalHolder) {
 
     $.get(urlPath, data => {
 
         $(modalHolder).html(data);
         $(modalHolder).find('.modal').modal('show');
+        $.validator.unobtrusive.parse($('form'));
     })
 
-    };
+};
+
+
+function submitForm() {
+    alert('valid');
+    if ($('form').valid()) {
+        alert('valid');
+    }
+}
