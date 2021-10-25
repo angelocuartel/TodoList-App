03aee5ce6005009ba1c10077fdd3555070e1d7c2
angelocuartel <68045263+angelocuartel@users.noreply.github.com>
2021-10-25 15:12:48 +0800

    add validation in todo POCO

---
+++ TodoList-App/Model Validations/TodoValidation.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList_App.Models;
+
+namespace TodoList_App.Model_Validations
+{
+    public class TodoValidation : AbstractValidator<Todo>
+    {
+        public TodoValidation()
+        {
+            RuleFor(x => x.TodoTask)
+                .NotEmpty()
+                .WithMessage("Please put some Task you lazy!")
+                .MaximumLength(20)
+                .WithMessage("you only have 20 max characters");
+                
+        }
+    }
+}
